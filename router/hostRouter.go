45044f618e441425ff33738719d4bb6f59d3e5a3
Xantios Krugor <git@xantios.nl>
2021-08-29 16:36:04 +0200

    Make sure X-Headers are set properly

--- router/hostRouter.go
+++ router/hostRouter.go
@@ -14,33 +14,30 @@ func RouteHost(req *http.Request,res http.ResponseWriter,route Route) {
 	// The request we are going to send to the server
 	reverseProxy.Director = func(req *http.Request) {
 
-		// main.logger.Debug("Director")
+		// destinationUrl.Host
 
 		// Be a good person, make sure X-Proxy headers are set correctly.
 		req.Header.Add("X-Forwarded-Host", req.Host)
-		req.Header.Add("X-Origin-Host", destinationUrl.Host)
-		req.URL.Scheme = "http"
+		req.Header.Add("X-Origin-Host", req.Host)
+		req.URL.Scheme = destinationUrl.Scheme
 		req.URL.Host = destinationUrl.Host
+		req.Host = destinationUrl.Host
+
+		if debug {
+			LogRequest(req)
+		}
 	}
 
 	reverseProxy.ModifyResponse = func(resp *http.Response) error {
 
-		// if debug {
-
-			//logger.Debug("Response from host")
-			//logger.Debug("\t"+resp.Proto+" "+resp.Status)
-			//logger.Debug("")
-
-			//for key,_ := range resp.Header {
-				// logger.Debug("\t"+key,":",resp.Header.Get(key))
-			// }
-		//}
+		if debug {
+			LogResponse(resp)
+		}
 
 		// No errors ! Let's go!
 		return error(nil)
 	}
 
 	reverseProxy.ErrorHandler = GenericErrorHandler
-
 	reverseProxy.ServeHTTP(res,req)
 }
